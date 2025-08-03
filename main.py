d18335b9365fb02c2c8eb495066a4a7c5c32e85a
Jim Snavely <jim@radixhealth.io>
2025-08-03 18:38:46 -0400

    load and save the world

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from ursina import scene, raycast, camera, mouse, destroy, color, Button, Ursina, held_keys, time, Entity, application
 from ursina.prefabs.first_person_controller import FirstPersonController
 import random
+import pickle
+import os
 
 app = Ursina()
 
@@ -39,16 +41,56 @@ class Voxel(Button):
             highlight_color=color.lime,
         )
 
+voxels = []
+
+def save_world(filename='world.pkl'):
+    world_data = {
+        'voxel_positions': [voxel.position for voxel in voxels],
+        'voxel_colors': [voxel.color for voxel in voxels],
+        'player_position': player.position,
+        'has_gravity': app.has_gravity
+    }
+    with open(filename, 'wb') as f:
+        pickle.dump(world_data, f)
+    print(f"World saved to {filename}")
+
+def load_world(filename='world.pkl'):
+    if not os.path.exists(filename):
+        print(f"Save file {filename} not found")
+        return
+    
+    for voxel in voxels[:]:
+        destroy(voxel)
+    voxels.clear()
+    
+    with open(filename, 'rb') as f:
+        world_data = pickle.load(f)
+    
+    for pos, col in zip(world_data['voxel_positions'], world_data['voxel_colors']):
+        voxel = Voxel(position=pos)
+        voxel.color = col
+        voxels.append(voxel)
+    
+    player.position = world_data['player_position']
+    app.has_gravity = world_data['has_gravity']
+    player.gravity = 1 if app.has_gravity else 0
+    
+    print(f"World loaded from {filename}")
+
 for z in range(20):
     for x in range(20):
         voxel = Voxel(position=(x,0,z))
+        voxels.append(voxel)
 
 def input(key):
     if key == 'left mouse down':
         hit_info = raycast(camera.world_position, camera.forward, distance=5)
         if hit_info.hit:
-            Voxel(position=hit_info.entity.position + hit_info.normal)
+            new_voxel = Voxel(position=hit_info.entity.position + hit_info.normal)
+            voxels.append(new_voxel)
     if key == 'right mouse down' and mouse.hovered_entity:
+        if mouse.hovered_entity in voxels:
+            voxels.remove(mouse.hovered_entity)
         destroy(mouse.hovered_entity)
     if key == 'escape':
         quit()
@@ -65,6 +107,12 @@ def input(key):
             player.gravity = 1
         else:
             player.gravity = 0
+    
+    if key == 'k':
+        save_world()
+    
+    if key == 'l':
+        load_world()
 
 app.run()
 
