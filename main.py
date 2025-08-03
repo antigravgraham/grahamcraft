94aa0a2c2cc3351830b50cfdb5bbcf1e18ef1631
Graham <graham@macbookair.mynetworksettings.com>
2025-08-03 13:04:43 -0400

    use arrow keys

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-
-from ursina import scene,raycast,camera,mouse,destroy,color, Button, Ursina
+from ursina import scene, raycast, camera, mouse, destroy, color, Button, Ursina, held_keys, time
 from ursina.prefabs.first_person_controller import FirstPersonController
 import random
 
 app = Ursina()
 
-player = FirstPersonController(gravity=1)
-app.has_gravity=True
+class ArrowKeyController(FirstPersonController):
+    def update(self):
+        speed = self.speed * time.dt
+        if held_keys['up arrow']:
+            self.position += self.forward * speed
+        if held_keys['down arrow']:
+            self.position -= self.forward * speed
+        if held_keys['left arrow']:
+            self.position -= self.right * speed
+        if held_keys['right arrow']:
+            self.position += self.right * speed
+        super().update()
+
+player = ArrowKeyController(gravity=1)
+app.has_gravity = True
 
 class Voxel(Button):
     def __init__(self, position=(0,0,0)):
@@ -23,7 +35,6 @@ for z in range(8):
     for x in range(8):
         voxel = Voxel(position=(x,0,z))
 
-
 def input(key):
     if key == 'left mouse down':
         hit_info = raycast(camera.world_position, camera.forward, distance=5)
@@ -36,11 +47,9 @@ def input(key):
     if key == 'g':
         app.has_gravity = not app.has_gravity
         if app.has_gravity:
-            player.gravity=1
+            player.gravity = 1
         else:
-            player.gravity=0
-
-
+            player.gravity = 0
 
 app.run()
 
